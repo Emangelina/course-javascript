a6ee71b794bd06c3b6b27744e0e573e80efa7ead
Tas Zaitseva <tas.zaitseva@gmail.com>
2022-05-09 14:09:59 +1000

    сделала задание 2 недели 2

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,17 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+
+// function forEach(array, fn) {
+//   for(let item of array) {
+//     fn(item);
+//   }
+// }
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 
 /*
  Задание 2:
@@ -20,7 +30,21 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+
+// function map(array, fn) {
+//   let resultArray = [];
+//   for(let item of array) {
+//     resultArray.push(fn(item));
+//   }
+//   return resultArray;
+// }
+function map(array, fn) {
+  const resultArray = [];
+  for (let i = 0; i < array.length; i++) {
+    resultArray.push(fn(array[i], i, array));
+  }
+  return resultArray;
+}
 
 /*
  Задание 3:
@@ -31,7 +55,32 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
+// function reduce(array, fn, initial=0) {
+//   let acc = initial;
+//   for (let item of array) {
+//     acc = fn(acc, item);
+//   }
+//   return acc;
+// }
+
+// function reduce(array, fn, initial=0) {
+//   let acc = initial;
+//   for (let i=0; i<array.length; i++) {
+//     acc = fn(acc, array[i], i, array);
+//   }
+//   return acc;
+// }
+
+function reduce(array, fn, initial) {
+  const hasInitial = typeof initial !== 'undefined'; //если не задали значение по умолчанию, то нужно проверить, передали initial или нет. Данное выражение вернет true, если initial было передано
+  let acc = hasInitial ? initial : array[0]; //проверяем, если hasInitial true(initial был передан), то значение переменной-аккумулятора будет равно initial. Если false(initial не передан), то первый элемент массива(с индексом 0)
+
+  for (let i = hasInitial ? 0 : 1; i < array.length; i++) {
+    //нужно назначить i в зависимости от значения переменной-аккумулятора.
+    acc = fn(acc, array[i], i, array);
+  }
+  return acc;
+}
 
 /*
  Задание 4:
@@ -41,7 +90,13 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const resultArray = [];
+  for (const item in obj) {
+    resultArray.push(item.toUpperCase());
+  }
+  return resultArray;
+}
 
 /*
  Задание 5 *:
@@ -54,6 +109,15 @@ function upperProps(obj) {}
    obj.foo = 2;
    console.log(obj.foo); // 4
  */
-function createProxy(obj) {}
+function createProxy(obj) {
+  const handler = {
+    set: (obj, name, value) => {
+      obj[name] = value ** 2;
+      return true; //вот тут нужно обязательно возвращать true
+    },
+  };
+  const proxy = new Proxy(obj, handler);
+  return proxy;
+}
 
 export { forEach, map, reduce, upperProps, createProxy };
